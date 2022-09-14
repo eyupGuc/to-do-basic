fede55649c8b7e0d9366cd0f61d5f29cc3462432
eyupGuc <meydanguresi@gmail.com>
2022-09-14 06:23:49 +0300

    add event handler changed

---
+++ 2-event-example/app.js
@@ -0,0 +1,20 @@
+//? Selectors
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
+
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+
+//? addBtn event handler
+
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a task");
+  } else {
+    newUl.innerHTML += `<li> ${langInput.value} </li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
